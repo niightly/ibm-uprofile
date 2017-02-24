e1bdf473015eb6631a8d01656e8c2da58449f3e7
thiagogs <thiagogs@br.ibm.com>
2017-02-24 15:11:49 -0300

    Removing the needs to usee methods on this module

--- index.js
+++ index.js
@@ -1,88 +1,57 @@
 const request = require('request')
-const URL = 'https://w3-services1.w3-969.ibm.com/myw3/unified-profile/v1/docs/instances/master?userId='
-var template = undefined
-var errorMsg = ""
-const headers = ['modifiedDate', 'userId', 'typeId', 'active']
-
-Object.getByStringKey = function(obj, key) {
-    let array = key.split('.');
-    for (let property of array) {
-
-    	if (property in obj) { obj = obj[property] }
-    	else { return; }
-    }
-
-    return obj;
-}
-
-Object.isValid = function(obj) {
-	if (!obj) { return true }
-
-	console.log(typeof obj === "string", obj);
-	if (typeof obj === "string" && obj.toLowerCase()==="default") { return true }
-	else if (typeof obj !== 'object' || Array.isArray(obj)) { 
-		errorMsg = { 
-			code: 400, 
-			name: "InvalidTemplate", 
-			message: "INVALID_TEMPLATE_TYPE", 
-			stack: (Array.isArray(obj)) ? "array" : typeof obj 
-		}
-		return false 
-	}
-
-	let isInvalid = new RegExp(/[^a-zA-Z0-9\.]/)
-	for (let key in obj) {
-		if (isInvalid.test(obj[key])) {
-			errorMsg = { 
-				code: 400, 
-				name: "InvalidTemplate", 
-				message: "INVALID_PROPERTY_" + key.toUpperCase(),
-				stack: "Please use only Alphanumeric characters or dots. Invalid entry: " + obj[key]
-			}
-			return false 
-		}
-	}
-	return true
-}
+var self
 
 class UProfile {
-	constructor(){}
-
-	static setDefault(userTemplate){
-		if (!Object.isValid(userTemplate)) {
-			let warningMsg = '\n' + '#'.repeat(23) + ' WARNING ' + '#'.repeat(23) + '\n'
-			console.log('#'.repeat(55) + warningMsg)
-			console.log(errorMsg);
-			console.log(warningMsg + '#'.repeat(55) + '\n')
-		}
-		template = userTemplate
+	constructor(){
+		this.URL = 'https://w3-services1.w3-969.ibm.com/myw3/unified-profile/v1/docs/instances/master?userId='
+		this.headers = ['modifiedDate', 'userId', 'typeId', 'active']
+		this.template = undefined
+		this.errorMsg = ""
+		self = this //to keep reference of the instantiated class and still dont need to call methods
 	}
 
-	static get(userUID, userTemplate) {
+	setDefault(userTemplate){
+		if (!this._isValid(userTemplate)) {
+			let warningMsg = '\n' + '#'.repeat(23) + ' WARNING ' + '#'.repeat(23) + '\n'
+			console.log('#'.repeat(55) + warningMsg)
+			console.log(this.errorMsg);
+			console.log(warningMsg + '#'.repeat(55) + '\n')
+			throw new Error(this.errorMsg)
+		}
+		this.template = userTemplate
+	}
+
+	get(userUID, userTemplate) {
 		return new Promise((resolve,reject) => {
 			try {
-				let lookup = (!userTemplate) ? template : userTemplate
-				if (!Object.isValid(lookup)) { return reject(errorMsg) }
+				let lookup = (!userTemplate) ? self.template : userTemplate
+				if (!self._isValid(lookup)) { return reject(self.errorMsg) }
 
-				request.get({url: URL + userUID, json: true}, function(err, response, body) {
-					if (err || !(response && response.statusCode == 200)) { 
-						return reject({ code: response.statusCode, name: "ServerError", message: "REQUEST_ERROR", stack: err }) }
-					else if (!body || !body.content || !body.content.identity_info) { 
+				request.get({url: self.URL + userUID, json: true}, function(err, response, body) {
+					if (err || !(response && response.statusCode == 200)) {
+						if (response && response.statusCode) {
+							return reject({ code: response.statusCode, name: "ServerError", message: "REQUEST_ERROR", stack: err })
+						} else {
+							let message = "REQUEST_ERROR"
+							if (err.code === "ENOTFOUND") { message = "NOT_IN_THE_VPN" }
+							return reject({ code: 500, name: "ServerError", message: message, stack: err })
+						}
+					} else if (!body || !body.content || !body.content.identity_info) { 
 						return reject({ code: 404, name: "ServerError", message: "ENTRY_NOT_FOUND", stack: body }) }
 
 					let user = {}
 					if (lookup && typeof lookup !== "string") {
-						let uContent = Object.getByStringKey(body, 'content.identity_info')
+						let uContent = self._getByStringKey(body, 'content.identity_info')
 						delete body.content
 						let uHeader = body
 
 						uContent.nameDisplay = (!uContent.nameDisplay) ? uContent.nameFull : uContent.nameDisplay
 
 						for (let property in lookup) {
-							if (headers.indexOf(lookup[property]) > 0) {
-								user[property] = Object.getByStringKey(uHeader, lookup[property])
+							if (self.headers.indexOf(lookup[property]) > 0) {
+								user[property] = self._getByStringKey(uHeader, lookup[property])
 							} else {
-								user[property] = Object.getByStringKey(uContent, lookup[property])
+								user[property] = self._getByStringKey(uContent, lookup[property])
 							}
 						}
 
@@ -101,6 +70,55 @@ class UProfile {
 			}
 		})
 	}
+
+	_getByStringKey(obj, key) {
+		let array = key.split('.');
+		for (let property of array) {
+
+			if (property in obj) { obj = obj[property] }
+			else { return; }
+		}
+
+		return obj;
+	}
+
+	_isValid(obj) {
+		if (!obj) { return true }
+
+		if (typeof obj === "string" && obj.toLowerCase()==="default") { return true }
+		else if (typeof obj !== 'object' || Array.isArray(obj)) { 
+			this.errorMsg = { 
+				code: 400, 
+				name: "InvalidTemplate", 
+				message: "INVALID_TEMPLATE_TYPE", 
+				stack: (Array.isArray(obj)) ? "array" : typeof obj 
+			}
+			return false 
+		}
+
+		let isInvalid = new RegExp(/[^a-zA-Z0-9\.]/)
+		for (let key in obj) {
+			if (isInvalid.test(obj[key])) {
+				this.errorMsg = { 
+					code: 400, 
+					name: "InvalidTemplate", 
+					message: "INVALID_PROPERTY_" + key.toUpperCase(),
+					stack: "Please use only Alphanumeric characters or dots. Invalid entry: " + obj[key]
+				}
+				return false 
+			}
+		}
+		return true
+	}
 }
 
-module.exports = UProfile
+/**
+ * Allow to return the module instantiated without using new when require the module.
+ */
+const instance = new UProfile()
+function instantiate(attributes) {
+  instance.setDefault(attributes)
+  return instance.get
+}
+
+module.exports = instantiate
